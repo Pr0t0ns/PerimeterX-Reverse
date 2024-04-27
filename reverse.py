b1229b5c730948bafbe52c409323685ce8758554
Pr0t0n <105520163+Pr0t0ns@users.noreply.github.com>
2024-04-27 11:59:21 -0400

    Create reverse.py

---
+++ reverse.py
@@ -0,0 +1,24 @@
+
+# First Collector POST Request Payload UnEncrypted
+payload_unencrypted = [{
+    "t": "PX12095", 
+    "d": {
+        "PX11645": "https://www.ssense.com/en-ca", # Endpoint/Host
+        "PX12207": 0, # To Be Determined (Probably False Value)
+        "PX12458": "Win32", # OS
+        "PX11902": 0, # To Be Determined (Probably False Value)
+        "PX11560": 756, # Browser Width (Not 100% Sure) 
+        "PX12248": 3600, # Browser Height (Not 100% Sure)
+        "PX11385": 1713737306091, # Unix Runtime (Start Time)
+        "PX12280": 1713737306096, # Unix Runtime (End Time)
+        "PX11496": "ad2e4860-002b-11ef-bc61-1b44c38fe285", # UUID (this is important for reverse) 
+        "PX12564": None,
+        "PX12565": -1, # TO BE DETERMINED (probably False Value or Undefined)
+        "PX11379": True # TO BE DETERMINED (Probably True Value)
+    }
+}]
+
+
+
+
+

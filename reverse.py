6a53386549e20f419622d7d519d171b4f692cfa5
Pr0t0n <105520163+Pr0t0ns@users.noreply.github.com>
2024-04-27 12:09:00 -0400

    Update reverse.py
    Reversed "Payload" Value 
    
    Now need to reverse "pc" value which is a PoC

--- reverse.py
+++ reverse.py
@@ -1,3 +1,11 @@
+import json
+from urllib.parse import quote, unquote
+import re
+import base64
+import math
+
+# For Padding (Pretty sure it's a static value globally)
+character_list = list("G^S}DNK8DNa>D`K}GK77")
 
 # First Collector POST Request Payload UnEncrypted/Decoded
 payload_unencrypted = [{
@@ -18,7 +26,57 @@ payload_unencrypted = [{
     }
 }]
 
+def Sl(t,e,n,r,a) -> int:
+    return math.floor((t - e) / (n - e) * (a - r) + r)
+
+def encode(payload: str, e:int=50) -> str:
+    output = ''
+    for i in range(len(payload)):
+        output += chr(ord(payload[i]) ^ e)
+    return output
+
+def encrypt(payload: str) -> str:
+    encoded_str = quote(payload)
+    decoded_str = unquote(re.sub(r'%([0-9A-F]{2})', lambda m: chr(int(m.group(1), 16)), encoded_str))
+    return base64.b64encode(decoded_str.encode()).decode()
+
+def get_offset(t, e, n) -> list:
+    g = encode(base64.b64encode(str(n).encode()).decode(), 10)
+    p = []
+    w = -1
+
+    for A in range(len(t)):
+        m = math.floor(A / len(g) + 1)
+        y = A if A >= len(g) else A % len(g)
+        G = ord(g[y]) * ord(g[m])
+        if G > w:
+            w = G
+    for V in range(len(t)):
+        b = math.floor(V / len(g)) + 1
+        Q = V % len(g)
+        T = ord(g[Q]) * ord(g[b])
+        
+        if T >= e:
+            T = Sl(T, 0, w, 0, e - 1)
+        
+        while T in p:
+            T += 1
+        
+        p.append(T)
+    return sorted(p)
+
+def add_padding(payload: str, PX_UID: str) -> str:
+    n = get_offset(character_list, 368, PX_UID)
+    i = 0; o = ""; c = character_list
+    for u in range(20):
+        o += payload[i:n[u] - u - 1] + c[u]
+        i = n[u] - u - 1
+    return o + payload[i:]
 
 
-
-
+# Generate the Encrypted "Payload" value.
+PXUID = payload_unencrypted[0]['d']['PX11496']
+payload_unencrypted = json.dumps(payload_unencrypted).replace(" ", "")
+encrypted_payload = (encrypt(encode(payload_unencrypted)))
+finished_payload = (add_padding(encrypted_payload, PXUID))
+print(finished_payload)
